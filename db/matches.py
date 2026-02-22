b9fd3ad812fa55dec29f1a55b55968fb59a031b9
remo <zyduremik@gmail.com>
2026-02-22 14:41:04 +0100

    baza danych

---
+++ db/matches.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class Match(Base):
+    __tablename__ = "matches"
+
+    id = Column(Integer,primary_key=True)
+
+    league_id = Column(Integer, ForeignKey("leagues.id"))
+    home_team_id = Column(Integer, ForeignKey("teams.id"))
+    away_team_id = Column(Integer, ForeignKey("teams.id"))
+
+    match_date = Column(DateTime)
+
+    home_score = Column(Integer)
+    away_score = Column(Integer)
+
+    home_shots_on_target = Column(Integer)
+    away_shots_on_target = Column(Integer)
+
+    home_possesion = Column(Integer)
+    away_possesion = Column(Integer)
+
+    home_corners = Column(Integer)
+    away_coners = Column(Integer)
+
+    home_yellow_cards = Column(Integer)
+    away_yellow_cards = Column(Integer)
+
+    league = relationship("League")
+    home_team = relationship("Team", foreign_keys=[home_team_id])
+    away_team = relationship("Team", foreign_keys=[away_team_id])
